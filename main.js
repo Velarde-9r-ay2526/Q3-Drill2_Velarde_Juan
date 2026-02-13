8ba02df0471ce8c86ae9b89838791bbf3f07c21f
Velarde-9r-ay2526 <velarde.jm@obmontessori.edu.ph>
2026-02-13 13:18:07 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function getSum(){
+            let b = document.getElementById("base").value;
+            let h = document.getElementById("height").value;
+            let ans =  (b * h) / 2;
+
+            document.getElementById("result").value = ans;
+        }
